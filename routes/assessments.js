ba8c6013a05e6fd91c103b4b8036c79fe4cbf596
geethasandesh <120577999+geethasandesh@users.noreply.github.com>
2026-02-23 16:25:13 +0530

    excel and passport error fix

--- routes/assessments.js
+++ routes/assessments.js
@@ -643,6 +643,7 @@ router.get('/tests/:id/test', async (req, res) => {
       })
     }
     const safe = questions.map(q => {
+      // eslint-disable-next-line no-unused-vars -- omit correctAnswer from client response
       const { correctAnswer, ...rest } = q
       return rest
     })
